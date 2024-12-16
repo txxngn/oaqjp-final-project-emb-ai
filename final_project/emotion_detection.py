5e2a8c51e538f04954c7f6199979b74f020cbf19
Thai Nguyen <thainguyen20594@gmail.com>
2024-12-15 21:41:31 -0500

    format output json

--- final_project/emotion_detection.py
+++ final_project/emotion_detection.py
@@ -8,4 +8,29 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
     response = requests.post(url,json=myobj,headers=header)
-    return response.text
+    formatted_response = json.loads(response.text) 
+
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    emotion_scores = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+    
+    return {    
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
