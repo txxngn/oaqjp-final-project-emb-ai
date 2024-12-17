d8173581700cbcbb084107817524f4437373bc8f
Thai Nguyen <thainguyen20594@gmail.com>
2024-12-16 19:43:44 -0500

    deploy using Flask

--- final_project/server.py
+++ final_project/server.py
@@ -9,16 +9,6 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector") #This path must match with path in mywebscript.js
 def sent_detector():
-    """
-    This function handles the '/emotionDetector' route. It receives the text input
-    from the user, passes it to the emotion_detector function, and processes the
-    returned response. The function then formats and returns a string containing
-    the emotion scores (anger, disgust, fear, joy) and the dominant emotion.
-    
-    Returns:
-        str: A formatted string containing the emotion scores and dominant emotion
-        or an error message if the dominant emotion is None.
-    """
 
     #Get the text input from the user, 'textToAnalyze' is the query parameter
     text_to_analyze = request.args.get('textToAnalyze')
@@ -39,17 +29,10 @@ def sent_detector():
         return "Invalid text! Please try again!"
     else:
         # Return a formatted response with the emotion scores and dominant emotion
-        return "For the given statement, the system response is 'anger':{}, 'disgust':{},'fear':{},'joy':{}. The dominant emotion is {}".format(anger_score, disgust_score, fear_score, joy_score, dominant_emotion)     
+        return "For the given statement, the system response is 'anger':{}, 'disgust':{}, 'fear':{}, 'joy':{}. The dominant emotion is {}".format(anger_score, disgust_score, fear_score, joy_score, dominant_emotion)     
      
 @app.route("/")
 def render_index_page():
-    """
-    This function handles the '/' route. It renders and returns the index page
-    (index.html).
-    
-    Returns:
-        str: The rendered HTML of the index page.
-    """
     return render_template('index.html')
 
 
